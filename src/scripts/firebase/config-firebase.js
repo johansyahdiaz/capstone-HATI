f4b9c37c518a3eb81df96607ac6c8925d2fc83b1
Johansyah Diaz <johansyah.diaz8@gmail.com>
2023-05-27 11:29:15 +0700

    Update

--- src/scripts/firebase/config-firebase.js
+++ src/scripts/firebase/config-firebase.js
@@ -1,25 +1,25 @@
-import firebase from 'firebase-admin'
+import firebase from 'firebase-admin';
 
-import serviceAccount from '../../../serviceAccountKey.json'
+import serviceAccount from '../../../serviceAccountKey.json';
 
 const firebaseConfig = {
 
-    credential: firebase.credential.cert(serviceAccount),
+  credential: firebase.credential.cert(serviceAccount),
 
-    apiKey: "AIzaSyBDkD6qUSeF_V3t9V3XwZ25CW1a1EI1cpQ",
-  
-    authDomain: "hati-35191.firebaseapp.com",
-  
-    databaseURL: "https://hati-35191-default-rtdb.asia-southeast1.firebasedatabase.app",
-  
-    projectId: "hati-35191",
-  
-    storageBucket: "hati-35191.appspot.com",
-  
-    messagingSenderId: "173681181765",
-  
-    appId: "1:173681181765:web:3a1c90b967b135b3424f2c"
-  
-  };
-  
-  export default firebaseConfig
+  apiKey: 'AIzaSyBDkD6qUSeF_V3t9V3XwZ25CW1a1EI1cpQ',
+
+  authDomain: 'hati-35191.firebaseapp.com',
+
+  databaseURL: 'https://hati-35191-default-rtdb.asia-southeast1.firebasedatabase.app',
+
+  projectId: 'hati-35191',
+
+  storageBucket: 'hati-35191.appspot.com',
+
+  messagingSenderId: '173681181765',
+
+  appId: '1:173681181765:web:3a1c90b967b135b3424f2c',
+
+};
+
+export default firebaseConfig;
